67a1f8803a2471c1743d26d700080a6c79a0acb1
Gowri-Arun <208835626+Gowri-Arun@users.noreply.github.com>
2025-07-24 15:43:28 +0530

    updated sign in page

---
+++ src/data/mockUsers.js
@@ -0,0 +1,13 @@
+// mockUsers.js
+const mockUsers = [
+  {
+    email: "Loisbecket@gmail.com",
+    password: "test1234",
+  },
+  {
+    email: "john.doe@example.com",
+    password: "securepass",
+  }
+];
+
+export default mockUsers;
